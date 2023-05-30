10d8f34c27afe8b2eac122950a129b545e1bdd9e
neeraj-kumar-30097672 <neeraj.kumar@adani.com>
2023-05-30 15:45:22 +0530

    Test 1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React from "react";
 
 export default function Navbar(props) {
   return (
     <div className={`navbar ${props.style}`}>
       <h2>LOGO</h2>
-      <div className='navlinks'>
+      <div className="navlinks">
+        <p>Link 1</p>
+        <p>Link 2</p>
+        <p>Link 3</p>
         <p>Link 1</p>
         <p>Link 2</p>
         <p>Link 3</p>
       </div>
       <button onClick={props.toggle}>Change Theme</button>
     </div>
-  )
+  );
 }
